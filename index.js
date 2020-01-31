1ac5ad79ed6a31edc8a10badcbdf52f5a27855df
Simo Sirén <45048404+Point-SimoSiren@users.noreply.github.com>
2020-01-31 03:15:58 +0200

    Laitettu esimerkkikoodi githubiin

---
+++ index.js
@@ -0,0 +1,14 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+// Tehdään polkumääritys public kansioon 
+const polku = path.join(__dirname, './public')
+
+// Sanotaan että em. polussa on tiedostosisältö jota palvelin käyttää kun se saa http requestin
+app.use(express.static(polku))
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+
+// Tämä on selaimen pään javascript tiedosto, joka on eri kuin node
+// tai express palvelin joka on kirjoitettu javascriptilla
+
+console.log('Selaimen Javascript on myös ladattu')
