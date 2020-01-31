3f911001c74829debe9ae5e9678e4b61deba1374
Simo Sirén <45048404+Point-SimoSiren@users.noreply.github.com>
2020-01-31 16:53:19 +0200

    jep jep

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,4 @@
 // Tämä on selaimen pään javascript tiedosto, joka on eri kuin node
 // tai express palvelin joka on kirjoitettu javascriptilla
 
-// json tiedoston lukeminen samasta kansiosta, eri tiedostosta
-var json = require('./data.json');
-
 console.log('Selaimen Javascript on myös ladattu')
